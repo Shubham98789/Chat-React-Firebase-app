910c4a307fd53905c78fb17d2f62c979ea88eb2b
Shubham Panwar <shubham526527@gmail.com>
2024-02-29 18:38:50 +0530

    register , login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Style.scss';
 import Register from './Pages/Register';
 
 function App() {
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import Add from "../Img/addAvatar.png"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">logo name</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="display name"/>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          <input style={{display:"none"}} type="file" id='file' />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+        </form>
+        <p>You have an account? Login</p>
+      </div>
+    </div>
   )
 }
 
