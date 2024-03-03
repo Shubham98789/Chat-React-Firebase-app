347d52a40f243463f1c2895d695cd08c3e8395e1
Shubham Panwar <shubham526527@gmail.com>
2024-03-03 21:16:05 +0530

    msg box

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Cam from "../Img/cam.png";
+import Add from "../Img/add.png";
+import More from "../Img/more.png";
+import Messages from "./Messages";
+import Input from "./Input"
 
 export default function Chat() {
   return (
-    <div className='chat'>
-      Chat
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input/>
     </div>
-  )
+  );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Img from "../Img/img.png"
+import Attach from "../Img/attach.png"
 
 export default function Input() {
   return (
-    <div>
-      
+    <div className='input'>
+      <input type="text" placeholder='Type Something...' />
+      <div className="send">
+        <img src={Img} alt="" />
+        <input type="file" style={{display:"none"}} id='file' />
+        <label htmlFor="file">
+          <img src={Attach} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
