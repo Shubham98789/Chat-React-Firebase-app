e5c3b1da6e7c2564236910c72a2200021937303b
dikshantkanela <dikshant7156@gmail.com>
2024-03-05 21:53:20 +0530

    remaining styles

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -6,11 +6,11 @@ export default function Chats() {
       <div className="userChat">
         <img
           src="https://images.unsplash.com/photo-1604004215402-e0be233f39be?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt=""
+          alt=""  
         />
         <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+            <span>Jane</span>
+            <p>Hello</p>
         </div>
       </div>
       
