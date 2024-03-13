55f552aa680b88a944e1e8474e23fa8d6a124b64
Shubham Panwar <shubham526527@gmail.com>
2024-03-13 17:47:28 +0530

    storage

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdi2-KvLbdkR3JKbLLANOKYbhKdSmo_XI",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
+export const storage = getStorage();
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Add from "../Img/addAvatar.png";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../Firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../Firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
 // image ko laane ke liye ye karte hai
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const user = userCredential.user;
-        console.log(user)
-        // ...
-        
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
   };
 
   return (
@@ -41,6 +54,7 @@ const Register = () => {
             <span>Add an avatar</span>
           </label>
           <button onSubmit={handleSubmit}>Sign up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You have an account? Login</p>
       </div>
